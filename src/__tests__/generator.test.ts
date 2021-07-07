7cfa3f3a7942e1a8a0a67f229233cc93793ca404
Dillon Schultz <dillonschultz93@gmail.com>
2021-07-06 20:02:39 -0700

    Create initial generator class

---
+++ src/__tests__/generator.test.ts
@@ -0,0 +1,66 @@
+import Generator from '../main/generator';
+import { WORDS } from '../constants/words';
+
+describe('Generator class', () => {
+  let generator: Generator;
+
+  beforeEach(() => {
+    generator = new Generator();
+  });
+
+  it('should be instantiated with the default options', () => {
+    const arrayEquals = (a: any[], b: any[]) =>  {
+      return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+    }
+
+    expect(generator.numberOfSentences).toEqual({ max: 7, min: 3});
+    expect(generator.numberOfWords).toEqual({max: 15, min: 5});
+    expect(arrayEquals(WORDS, generator.words)).toEqual(true);
+  });
+
+  it('should throw an error if the minimum number of sentences exceeds the maximum', () => {
+    try {
+      generator = new Generator({ numberOfSentences: { max: 2, min: 8 }});
+    } catch (error) {
+      expect(error).toBeDefined;
+      expect(error.message).toEqual('Minimum number of sentences per paragraph (8) cannot exceed the maximum (2)');
+    }
+  });
+
+  it('should throw an error if the minimum number of words exceeds the maximum', () => {
+    try {
+      generator = new Generator({ numberOfWords: { max: 2, min: 8 }});
+    } catch (error) {
+      expect(error).toBeDefined;
+      expect(error.message).toEqual('Minimum number of words per sentence (8) cannot exceed the maximum (2)');
+    }
+  });
+
+  it('should set the random property when passed a number', () => {
+    generator = new Generator({ random: 4 });
+
+    expect(generator.random).toEqual(4);
+  });
+
+  describe('createWords() Method', () => {
+    it('should create a specific number of random words', () => {
+      const assertion = generator.createWords(7);
+
+      expect(assertion.split(' ')).toHaveLength(7);
+    });
+
+    it('should create a random amount of words between the min and max range', () => {
+      const min = 5;
+      const max = 12;
+      generator = new Generator({ numberOfWords: { min, max }});
+
+      const assertion = generator.createWords();
+
+      expect(assertion.length <= max);
+      expect(assertion.length >= min);
+    });
+  });
+});
---
+++ src/main/generator.ts
@@ -0,0 +1,78 @@
+import { WORDS } from "../constants/words";
+import { capitalize, randomInt } from "../util";
+
+interface IRange {
+  min: number;
+  max: number;
+};
+
+interface IGeneratorOptions {
+  numberOfSentences?: IRange;
+  numberOfWords?: IRange;
+  random?: number;
+  words?: string[];
+}
+
+/**
+ * @description Generator class that provides methods to generate words and sentences.
+ */
+class Generator {
+  public numberOfSentences: IRange;
+  public numberOfWords: IRange;
+  public random: number;
+  public words: string[];
+
+  constructor({
+    numberOfSentences = { max: 7, min: 3 },
+    numberOfWords = { max: 15, min: 5 },
+    random,
+    words = WORDS
+  }: IGeneratorOptions = {}) {
+    // Check if minimum number of sentences exceeds maximum number.
+    if (numberOfSentences.min > numberOfSentences.max) {
+      throw new Error(
+        `Minimum number of sentences per paragraph (${
+          numberOfSentences.min
+      }) cannot exceed the maximum (${numberOfSentences.max})`);
+    }
+
+    // Check if the minimum number of words exceeds maximum number.
+    if (numberOfWords.min > numberOfWords.max) {
+      throw new Error(
+        `Minimum number of words per sentence (${
+          numberOfWords.min
+      }) cannot exceed the maximum (${numberOfWords.max})`);
+    }
+
+    this.numberOfSentences = numberOfSentences;
+    this.numberOfWords = numberOfWords;
+    this.random = random || Math.random();
+    this.words = words;
+  };
+
+  /**
+   * @description Picks a random word from the word array.
+   */
+  public randomWord(): string {
+    const index = randomInt(0, this.words.length - 1);
+    return this.words[index];
+  }
+
+  /**
+   * @description Generates an array of random words based on the WORDS array.
+   * @param num Number of words.
+   */
+  public createWords(num?: number): string {
+    const { min, max } = this.numberOfWords;
+    let lengthOfWords = num || randomInt(min, max);
+    let words = [];
+
+    for (let i = lengthOfWords; i > 0; i-- ) {
+      words.push(this.randomWord());
+    };
+
+    return words.join(' ');
+  }
+};
+
+export default Generator;
